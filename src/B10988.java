3384f52a621d2334631d414ecf56877bcdda852a
Daeyoul-Yang <cnrrnehfdl03@naver.com>
2023-10-27 08:56:30 +0900

    백준

---
+++ src/B10988.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class B10988 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s= sc.next();
+        boolean b =true;
+        char []ch = s.toCharArray();
+        for (int i = 0; i < ch.length/2; i++) {
+            if(ch[i]!=ch[ch.length-1-i]){
+                b=false;
+            }
+        }
+        if(b==false){
+            System.out.println(0);
+        }else {
+            System.out.println(1);
+        }
+
+    }
+}
---
+++ src/B1316.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class B1316 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int T= sc.nextInt();
+        boolean result =true;
+        int resultCount=0;
+        for (int test_case=1;test_case<=T;test_case++){
+            result=true;
+            String s= sc.next();
+            char []ch = s.toCharArray();
+            for (int i = 0; i < ch.length; i++) {
+                for (int j = i+1; j <ch.length ; j++) {
+                    if(ch[i]==ch[j]){
+                        if((j-i)>1){
+                            result=false;
+
+                        }break;
+                    }
+                }
+            }
+            if(result){
+                resultCount++;
+            }
+        }
+        System.out.println(resultCount);
+    }
+}
+// boolean 값 for 구문 돌때마다 초기화 잊지 않기!!
