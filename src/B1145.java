f04bcef9235205a3ef026431ae096938da773e3d
daeyeol-yang <yang980310@naver.com>
2024-03-15 17:57:53 +0900

    1094

--- src/B1145.java
+++ src/B1145.java
@@ -5,27 +5,27 @@ import java.util.StringTokenizer;
 
 public class B1145 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int [] arr = new int[5];
+        int[] arr = new int[5];
         for (int i = 0; i < arr.length; i++) {
-            arr[i]=Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
         int solution = 0;
-        int check =0;
-        while (true){
+        int check = 0;
+        while (true) {
             solution++;
 
             for (int i = 0; i < arr.length; i++) {
-                if(solution%arr[i]==0){
+                if (solution % arr[i] == 0) {
                     check++;
                 }
             }
 
-            if(check>=3){
+            if (check >= 3) {
                 break;
-            }else{
-                check=0;
+            } else {
+                check = 0;
             }
 
         }
