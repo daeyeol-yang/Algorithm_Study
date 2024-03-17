069e96cff259165f5379d1f00bf438a498fd80a2
daeyeol-yang <yang980310@naver.com>
2024-03-17 17:48:10 +0900

    1436

---
+++ src/B1436.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B1436 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Long n = (long) Integer.parseInt(br.readLine());
+        Long start = 0L;
+
+        while (true){
+            start++;
+            String s =String.valueOf(start);
+                if(s.contains("666")){
+                    n--;
+                }
+
+            if(n==0){
+                break;
+            }
+        }
+        System.out.println(start);
+    }
+}
