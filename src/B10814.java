62c684b35bee236178d0f695dad7fc6c9e2bad3c
daeyeol-yang <yang980310@naver.com>
2024-03-23 18:32:01 +0900

    1181

---
+++ src/B10814.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class B10814 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine());
+            int ageKey = Integer.parseInt(st.nextToken());
+            String nameValue = st.nextToken();
+
+
+        }
+    }
+}
