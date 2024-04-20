924e64bafc37182cf45010b4aa9f569116b2693c
daeyeol-yang <yang980310@naver.com>
2024-04-20 20:33:31 +0900

    2839

---
+++ src/B18111.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B18111 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int [][]map = new int[N][M];
+        int inventory = Integer.parseInt(st.nextToken());
+
+
+
+    }
+
+    public static void solution1(){
+
+    }
+}
--- src/B2839.java
+++ src/B2839.java
@@ -7,22 +7,25 @@ public class B2839 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int solution =0;
-        int solution1=0;
-        if(N%5==0){
-            solution = N/5 ;
-        }else{
-            int N1 = N%5;
-            solution1=N/5;
-            if(N1%3==0){
-                solution=N1/3+solution1;
+
+
+
+        while (true){
+            if(N%5==0){
+                solution+=N/5;
+                break;
+
             }else {
-                solution=-1;
+                N-=3;
+                solution++;
+                if(N<0){
+                    solution=-1;
+                    break;
+                }
             }
-
         }
+
         System.out.println(solution);
-
-
 
     }
 }
