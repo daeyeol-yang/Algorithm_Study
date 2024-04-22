d25dada7515c1f63f4ab337d5f3cc58d617aae68
daeyeol-yang <yang980310@naver.com>
2024-04-22 23:18:22 +0900

    18111

--- src/B18111.java
+++ src/B18111.java
@@ -12,18 +12,52 @@ public class B18111 {
         int M = Integer.parseInt(st.nextToken());
         int [][]map = new int[N][M];
         int inventory = Integer.parseInt(st.nextToken());
+        int max = 0;
+        int min = 257;
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
+                max = Math.max(max,map[i][j]);
+                min = Math.min(min,map[i][j]);
             }
         }
+        int low_count=Integer.MAX_VALUE;
+        int high =0;
 
+
+        for (int i = min; i <=max ; i++) {
+            int count =0;
+            int block = inventory;
+            for (int j = 0; j < N; j++) {
+                for (int k = 0; k < M; k++) {
+                    if(i>map[j][k]){
+                        count+=i-map[j][k];
+                        block-=i-map[j][k];
+                    }
+
+                    if(i<map[j][k]){
+                        count+=2*(map[j][k]-i);
+                        block+=map[j][k]-i;
+                    }
+
+                }
+            }
+            if(block<0){
+                continue;
+            }
+
+            if(count<=low_count){
+                high=i;
+                low_count=count;
+            }
+
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append(low_count).append(" ").append(high);
+        System.out.println(sb);
 
 
     }
 
-    public static void solution1(){
-
-    }
 }
