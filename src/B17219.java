269ee3f62be1be96329df5fcf8510b6345f3ef3b
daeyeol-yang <yang980310@naver.com>
2024-05-04 01:58:43 +0900

    17219

---
+++ src/B17219.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class B17219 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int T = Integer.parseInt(st.nextToken());
+        int T1 = Integer.parseInt(st.nextToken());
+        HashMap<String, String> hashMap =new HashMap<>();
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine());
+            hashMap.put(st.nextToken(), st.nextToken());
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < T1; i++) {
+            st= new StringTokenizer(br.readLine());
+            sb.append(hashMap.get(st.nextToken())).append('\n');
+        }
+
+        System.out.println(sb);
+
+
+    }
+}
