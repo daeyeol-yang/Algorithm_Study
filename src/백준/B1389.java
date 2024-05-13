d86b034455ebc612f75b7f9d32c659769316aac2
daeyeol-yang <yang980310@naver.com>
2024-05-13 19:25:21 +0900

    1389

--- src/백준/B1389.java
+++ src/백준/B1389.java
@@ -13,20 +13,53 @@ public class B1389 {
 
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
+        int [][]arr = new int[N+1][N+1];
+        int no = 100000000;
 
-        ArrayList<Integer>[] arr = new ArrayList[N];
-        for (int i = 0; i < N; i++) {
-            arr[i] = new ArrayList<>();
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <=N ; j++) {
+                if(i==j){
+                    arr[i][j]=0;
+                }else{
+                    arr[i][j]=no;
+                }
+            }
         }
 
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int M1 = Integer.parseInt(st.nextToken());
-            int M2 = Integer.parseInt(st.nextToken());
-            arr[M1].add(M2);
-            arr[M2].add(M1);
-
+        for (int i = 1; i <= M; i++) {
+            st= new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            arr[start][end]=1;
+            arr[end][start]=1;
         }
+
+        for (int i = 1; i <=N ; i++) {
+            for (int s = 1; s <=N ; s++) {
+                for (int e = 1; e <=N ; e++) {
+                    if(arr[s][e]>arr[s][i]+arr[i][e]){
+                        arr[s][e] = arr[s][i]+arr[i][e];
+                    }
+                }
+            }
+        }
+
+        int min = Integer.MAX_VALUE;
+        int answer=0;
+
+        for (int i = 1; i <=N ; i++) {
+            int sum=0;
+            for (int j = 1; j <=N ; j++) {
+                    sum+=arr[i][j];
+            }
+
+            if(sum<min){
+                min=sum;
+                answer=i;
+            }
+        }
+
+        System.out.println(answer);
 
 
     }
