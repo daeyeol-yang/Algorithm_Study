7d0623c4d56e857a51b75b3485892a2e50ecc245
daeyeol-yang <yang980310@naver.com>
2024-05-13 18:43:34 +0900

    11403

---
+++ src/백준/B11403.java
@@ -0,0 +1,44 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B11403 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int [][]arr = new int[N][N];
+        StringTokenizer st;
+        StringBuilder sb= new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int k = 0; k <N ; k++) {
+            for (int i = 0; i <N ; i++) {
+                for (int j = 0; j <N ; j++) {
+                    if(arr[i][k]==1 && arr[k][j]==1){
+
+                        arr[i][j]=1;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                sb.append(arr[i][j]).append(" ");
+            }
+            sb.append('\n');
+        }
+        System.out.println(sb);
+
+
+    }
+}
