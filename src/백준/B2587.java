7915b5779879ea203a6318cd78d76acc0142c622
daeyeol-yang <yang980310@naver.com>
2024-05-26 21:24:12 +0900

    2587

---
+++ src/백준/B2587.java
@@ -0,0 +1,21 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class B2587 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int [] arr = new int[5];
+        int sum =0;
+        for (int i = 0; i < 5; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            sum+= arr[i];
+        }
+        System.out.println(sum/5);
+        Arrays.sort(arr);
+        System.out.println(arr[2]);
+    }
+}
