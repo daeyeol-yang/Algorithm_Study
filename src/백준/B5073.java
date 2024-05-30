3003dbd8cc194155b8e289c370d8afe6cce5a0ff
daeyeol-yang <yang980310@naver.com>
2024-05-30 23:55:39 +0900

    5073

---
+++ src/백준/B5073.java
@@ -0,0 +1,19 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B5073 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true){
+            String s = br.readLine();
+            if(s.equals("0 0 0")){
+                break;
+            }
+
+        }
+    }
+}
