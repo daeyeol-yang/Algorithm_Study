1b315bc0dbd21f8875393f1e2a0579a9a1d41d7c
daeyeol-yang <yang980310@naver.com>
2024-06-13 18:24:15 +0900

    17413

---
+++ src/백준/B17413.java
@@ -0,0 +1,60 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class B17413 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+
+        Queue<Character> queue = new LinkedList<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            queue.add(c);
+
+        }
+        StringBuilder sb = new StringBuilder();
+        StringBuilder sb1 = new StringBuilder();
+        boolean check = false;
+
+        while (!queue.isEmpty()){
+            if(queue.peek()=='<'){
+
+                while (queue.peek()!='>'){
+                    sb.append(queue.poll());
+                }
+                sb.append(queue.poll());
+            }else {
+                if(!queue.isEmpty()) {
+                    while (queue.peek() != '<') {
+
+                        if(queue.peek()==' '){
+                            queue.poll();
+                            check=true;
+                            break;
+                        }
+                        sb1.append(queue.poll());
+
+                        if(queue.isEmpty()){
+                            break;
+                        }
+                    }
+
+                    sb.append(sb1.reverse());
+                    if(check){
+                        sb.append(" ");
+                        check=false;
+                    }
+                    sb1=new StringBuilder();
+                }
+            }
+
+        }
+        System.out.println(sb);
+    }
+}
