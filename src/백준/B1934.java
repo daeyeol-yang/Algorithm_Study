fb67cdb50ab53345f759aeaee1951de9d86c7a1b
daeyeol-yang <yang980310@naver.com>
2024-06-17 19:19:21 +0900

    1934

---
+++ src/백준/B1934.java
@@ -0,0 +1,29 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B1934 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            System.out.println(lcm(a,b));
+        }
+    }
+
+    public static int lcm(int a, int b){
+        return a*b/gcd(a,b);
+    }
+
+    public static int gcd(int a, int b){
+        if(b==0) return a;
+        return gcd(b,a%b);
+    }
+}
