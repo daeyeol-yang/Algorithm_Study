2e51646aa78bfc17d1d8aca6a49302dd1e1ae412
daeyeol-yang <yang980310@naver.com>
2024-06-27 02:47:07 +0900

    2558

---
+++ src/백준/B2558.java
@@ -0,0 +1,10 @@
+package 백준;
+
+import java.util.Scanner;
+
+public class B2558 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt()+sc.nextInt());
+    }
+}
