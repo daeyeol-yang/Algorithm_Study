f65bc75bbb72d38e87d66c086a334722560dc9da
daeyeol-yang <yang980310@naver.com>
2024-07-06 13:28:22 +0900

    11721

---
+++ src/백준/B11721.java
@@ -0,0 +1,20 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B11721 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            sb.append(s.charAt(i));
+            if((i+1)%10==0){
+                sb.append('\n');
+            }
+        }
+        System.out.println(sb);
+    }
+}
