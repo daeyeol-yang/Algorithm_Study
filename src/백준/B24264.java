6cc330fe7cc57feb699c18c2ab92e4ed396961bd
daeyeol-yang <yang980310@naver.com>
2024-07-15 15:41:26 +0900

    24264

---
+++ src/백준/B24264.java
@@ -0,0 +1,14 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B24264 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Long N = Long.parseLong(br.readLine());
+        System.out.println(N*N);
+        System.out.println(2);
+    }
+}
