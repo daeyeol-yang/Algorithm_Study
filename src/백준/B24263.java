7130ba827da31af43cd0ae6c194b71878a49123b
daeyeol-yang <yang980310@naver.com>
2024-07-15 15:33:10 +0900

    24263

---
+++ src/백준/B24263.java
@@ -0,0 +1,13 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B24263 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(Integer.parseInt(br.readLine()));
+        System.out.println(1);
+    }
+}
