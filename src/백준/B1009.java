dc310953b2353cf9de14e0c73cb2d35ffb42afd0
daeyeol-yang <yang980310@naver.com>
2024-07-18 13:38:48 +0900

    1009

---
+++ src/백준/B1009.java
@@ -0,0 +1,30 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B1009 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            long sol =1;
+            for (int j = 0; j < b; j++) {
+                sol=sol*a%10;
+                if(sol==0){
+                    sol=10;
+                }
+            }
+            sb.append(sol).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
